a8520455ecaa2831f824572056956301cf81c175
plpe2 <147789736+plpe2@users.noreply.github.com>
2025-04-24 23:04:49 +0800

    feat(register): transform registration form into dialog

--- src/assets/components/NavBar.tsx
+++ src/assets/components/NavBar.tsx
@@ -1,15 +1,29 @@
-import { AppBar, Button, Stack, Toolbar, Box, IconButton, TextField } from '@mui/material'
+import { AppBar, Button, Stack, Toolbar, Box, IconButton, TextField, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions } from '@mui/material'
 import { Link } from 'react-router-dom'
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import { useContext, useState } from 'react';
+import React, { useContext, useState } from 'react';
 import SearchIcon from '@mui/icons-material/Search';
-
+import axios from 'axios';
 
 function NavBar() {
+  type User = {
+    name: string,
+    email: string,
+    password: string,
+    cpassword: string
+  }
+
   const [sideBar, setSideBar] = useState(false)
   const [searchshow, setSearchShow] = useState(false)
   const [searchvalue, setSearchValue] = useState("")
-  
+  const [openRegister, setOpenRegister] = useState(false)
+
+  const [values, setValues] = useState<User>({
+    name: "",
+    email: "",
+    password: "",
+    cpassword: ""
+  })
 
   const toggleSidebar = () =>{
     setSideBar(prev => !prev)
@@ -19,9 +33,17 @@ function NavBar() {
     setSearchShow(prev => !prev)
   }
 
-  const handleSubmit = (e : React.FormEvent<HTMLFormElement>) =>{
+  const handleSearchSubmit = (e : React.FormEvent<HTMLFormElement>) =>{
     e.preventDefault()
     console.log(searchvalue)
+  }
+
+  const handleRegisterSubmit = (e : React.FormEvent<HTMLFormElement>) =>{
+    e.preventDefault()
+    axios.post("/register/user-create", values)
+    .then((res) => {
+      window.location.reload()
+    })  
   }
 
   const navStyle = {
@@ -37,7 +59,7 @@ function NavBar() {
       <Link to="/"><Button variant="contained" size="large" startIcon={<ShoppingCartIcon />}>Add2Cart</Button></Link>
       <Box sx={{ flexGrow: 1 }} />
       <Stack direction='row' height="50px">
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSearchSubmit}>
       <TextField size='small' onChange={(e) => setSearchValue(e.target.value)}>
           Search 
       </TextField>
@@ -47,8 +69,38 @@ function NavBar() {
       </form>
       </Stack>
       <Stack direction="row" spacing={2} className="Navbar">
-        <Link to="/login"><Button variant='contained'>Login</Button></Link>
-        <Link to="/register"><Button variant='contained'>Register</Button></Link>
+        <Link to="/login">
+          <Button variant='contained'>
+            Login
+          </Button>
+        </Link>
+        <Button variant='contained' color='success' onClick={() => setOpenRegister(true)}>
+          Register
+        </Button>
+
+        <Dialog open={openRegister} sx={{width: "100%"}} aria-labelledby='register-dialog'>
+          <DialogTitle>Registration</DialogTitle>
+          <DialogContentText></DialogContentText>
+          <form onSubmit={handleRegisterSubmit}>
+          <DialogContent >
+            <Stack spacing={2}>
+              <TextField label="Full Name" onChange={(e) => setValues({ ...values, name: e.target.value})}/>
+              <TextField label="Email Address" onChange={(e) => setValues({ ...values, email: e.target.value})}/>
+              <TextField label="Password" onChange={(e) => setValues({ ...values, password: e.target.value})}/>
+              <TextField label="Confirm Password" onChange={(e) => setValues({ ...values, cpassword: e.target.value})}/>
+            </Stack>
+          </DialogContent>
+          <DialogActions>
+            <Button variant="contained" color="success" type='submit'>
+              Register
+            </Button>
+            <Button variant="contained" color="error" onClick={() => setOpenRegister(false)}>
+              Cancel
+            </Button>
+          </DialogActions>
+          </form>
+        </Dialog>
+        {/* <Link to="/register"><Button variant='contained'>Register</Button></Link> */}
       </Stack>
       </Toolbar>
     </AppBar>
